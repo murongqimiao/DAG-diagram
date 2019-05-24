578514a331e90f3564eb19aae997da31014df88a
zhanglizhong@wecash.net <murongqimiao@live.cn>
2019-05-24 14:14:23 +0800

    feat: add correct fs

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'DAGBoard/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
     /**
